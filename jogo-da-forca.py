93a10b902fa088f35a2c24b973137ff8a5a7b371
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2023-02-27 19:27:51 -0300

    Update jogo-da-forca.py
    Atualização no script do Jogo da Forca.

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -1,76 +1,59 @@
+# Importando pacotes
 import random
-print("--------------------------- \n JOGO DA FORCA EM PYTHON \n \n Adivinhe o apelido do pênis: \n \n")
+import os
+import sys
+import time
 
-nomes = ['braulio', 'ganso', 'jeba', 'mastro', 'pau',
-         'pemba', 'pingola', 'pinto', 'pomba', 'rola', 'trolha']
-# escolhendo um nome aleatório
+# comando para limpar a tela
+limpa_tela = os.system('cls' if os.name == 'nt' else 'clear') # limpa a tela com cls no Windows e o comando clear em outros sistemas
+
+def reiniciar_game():
+    # limpa a tela e reinicia o programa
+    limpa_tela # limpa a tela com cls no Windows e o comando clear em outros sistemas
+    os.execv(sys.executable, ['python'] + sys.argv) # reinicia o programa
+
+# print no início do game
+print("\n --------------------------- \n JOGO DA FORCA EM PYTHON \n \n Adivinhe o nome do filme: \n \n")
+
+# lista de nomes de filmes
+nomes = ['alien', 'amelie', 'arrival', 'avatar', 'bambi', 'braveheart', 'casablanca', 'coco', 'dumbo', 'fargo', 'fantasia', 'frozen', 'gladiator', 'gravity', 'hereditary', 'hugo', 'interstellar', 'jumanji', 'matilda', 'memento', 'moana', 'mulam', 'psycho', 'scream', 'shrek', 'tangled', 'us', 'vertigo', 'whiplash', 'zootopia']
+
+# escolhendo um nome aleatório da lista de nomes de filmes
 nome_aleatorio = random.choice(nomes)
+
+# criando o nome oculto para exibir a quantidade de letras no prompt
 nome_oculto = "_" * len(nome_aleatorio)
-# variáveis do jogo
+
+# criando variáveis do game
 tentativas = 4
 letras_escolhidas = []
 game_over = False
 
 # função para atualizar palavra oculta ao acertar a letra
-
-
 def atualizar_oculta(oculta, letra, palavra):
-    nova_palavra_oculta = ""
+    novo_nome_oculto = ""
     for i in range(len(palavra)):
+        # se a letra na posição i for igual a letra que o usuário escolheu
         if palavra[i] == letra:
-            nova_palavra_oculta += letra
+            # adiciona a letra na exibição do novo nome oculto
+            novo_nome_oculto += letra
         else:
-            nova_palavra_oculta += oculta[i]
-    return nova_palavra_oculta
-
-# função para desenhar a forca
-def forca(tentativas):
-    vidas = 4 - tentativas
-    if vidas == 0:
-        print("  ________")
-        print(" |/       |")
-        print(" |")
-        print(" |")
-        print(" |")
-        print(" |")
-        print("_|________")
-    elif vidas == 1:
-        print("  ________")
-        print(" |/       |")
-        print(" |       (_)")
-        print(" |")
-        print(" |")
-        print(" |")
-        print("_|________")
-    elif vidas == 2:
-        print("  ________")
-        print(" |/       |")
-        print(" |       (_)")
-        print(" |       /|")
-        print(" |")
-        print(" |")
-        print("_|________")
-    elif vidas == 3:
-        print("  ________")
-        print(" |/       |")
-        print(" |       (_)")
-        print(" |       /|\\")
-        print(" |        |")
-        print(" |       / \\")
-        print("_|________")
-        
+            # se não, adiciona o caracter "_" no novo nome oculto
+            novo_nome_oculto += oculta[i]
+            # retorna o novo nome oculto após as verificações
+    return novo_nome_oculto
 
 # loop inicial do jogo
 while game_over != True:
-    forca(tentativas)
     print(nome_oculto)
-    print("\n Tentativas restantes: ", tentativas)
+    print("\nTentativas restantes: ", tentativas)
     print("Letras escolhidas: ", letras_escolhidas)
     letra = input("Escolha uma letra: ")
 
     # verificando as letras escolhidas
     if letra in letras_escolhidas:
         print("Burro! Já digitou essa letra antes!")
+        time.sleep(2)
     else:
         letras_escolhidas.append(letra)
         if letra in nome_aleatorio:
@@ -95,7 +78,10 @@ while game_over != True:
                 print(" |        |")
                 print(" |       / \\")
                 print("_|________")
-                print("\n Não, cara. Que loucura! Como vc é burro! \n A nome do pênis era: ", nome_aleatorio)
+                print("\n Não, cara. Que loucura! Como vc é burro! \n\n A nome do filme era: ", nome_aleatorio)
                 game_over = True
 
-
+# aguardar 5 segundos
+time.sleep(5)
+# reiniciar o game
+reiniciar_game()
